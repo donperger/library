312d683b833f886d771a4c36be4dc581e0c41c84
Rafael Perger <rafael.perger@gmail.com>
2022-02-26 17:21:22 +0100

    Implement displayBooks function. Create createBookCard function.

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 let library = [
-    {title: "Harry Potter and the Order of the Phoenix",
-    author: "J. K. Rowling",
-    numOfPages: 766,
-    isread: true
+    {
+        title: "Harry Potter and the Order of the Phoenix",
+        author: "J. K. Rowling",
+        numOfPages: 766,
+        isread: true
     },
-    {title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    numOfPages: 295,
-    isread: false
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        numOfPages: 295,
+        isread: false
     }
 ];
+
+const bookContainer = document.querySelector(".book-container");
+
+displayBooks();
 
 function Book(title, author, numOfPages, isread) {
     this.title = title
     this.author = author
     this.numOfPages = numOfPages
     this.isread = isread
-    this.info = function() {
-        if(isread === true) {
+    this.info = function () {
+        if (isread === true) {
             return `${this.title} by ${this.author}, ${this.numOfPages} pages, have read`
         } else {
             return `${this.title} by ${this.author}, ${this.numOfPages} pages, not read yet`
-        } 
+        }
     }
+}
+
+function displayBooks() {
+    library.map((book, i) => {
+        const bookCard = createBookCard(book.title, book.author, book.numOfPages, i+1)
+
+        bookContainer.appendChild(bookCard);
+    })
+}
+
+function createBookCard(title, author, numOfPages, number) {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book-card", number);
+
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("book-title");
+
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("book-info");
+
+    titleDiv.innerText = title;
+    infoDiv.innerText = `by ${author}, ${numOfPages} pages`;
+
+    bookDiv.append(titleDiv, infoDiv);
+
+    return bookDiv;
 }
