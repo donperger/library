534a830153a3dec28d0d3e1885df3001ecb78ebe
Rafael Perger <rafael.perger@gmail.com>
2022-02-27 17:24:50 +0100

    Add clearChild method to DOM element prototype. Create addBook function.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+if( typeof Element.prototype.clearChildren === 'undefined' ) {
+    Object.defineProperty(Element.prototype, 'clearChildren', {
+      configurable: true,
+      enumerable: false,
+      value: function() {
+        while(this.firstChild) this.removeChild(this.lastChild);
+      }
+    });
+}
+
 let library = [
     {
         title: "Harry Potter and the Order of the Phoenix",
@@ -20,7 +30,13 @@ const addNewBookButton = document.querySelector(".add-new");
 const addBookButton = document.querySelector(".add");
 const cancelButton = document.querySelector(".cancel");
 
+const titleInput = document.querySelector("#book_title");
+const authorInput = document.querySelector("#book_author");
+const numOfPagesInput = document.querySelector("#book_pages");
+const isReadInput = document.querySelector("#is_read")
+
 addNewBookButton.addEventListener("click", showAddFrom);
+addBookButton.addEventListener("click", addBook);
 cancelButton.addEventListener("click", hideAddFrom);
 
 displayBooks();
@@ -33,6 +49,8 @@ function Book(title, author, numOfPages, isread) {
 }
 
 function displayBooks() {
+    bookContainer.clearChildren();
+
     library.map((book, i) => {
         const bookCard = createBookCard(book.title, book.author, book.numOfPages, i+1)
 
@@ -60,8 +78,23 @@ function createBookCard(title, author, numOfPages, number) {
 
 function showAddFrom() {
     addFromContainer.style.display = "flex";
+    titleInput.focus();
 }
 
 function hideAddFrom() {
     addFromContainer.style.display = "none"
+}
+
+function addBook() {
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const numOfPages = numOfPagesInput.value;
+    const isread = isReadInput.checked;
+
+    const newBook = new Book(title, author, numOfPages, isread);
+
+    library.push(newBook);
+
+    hideAddFrom();
+    displayBooks();
 }
